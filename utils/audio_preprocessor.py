159a8031ee956cb8b109b8df18982e79597201b5
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2026-06-16 05:35:52 -0700

    Implement coversion logic

--- utils/audio_preprocessor.py
+++ utils/audio_preprocessor.py
@@ -1,10 +1,33 @@
+import os
+import sys
+
+if sys.platform == "win32":
+    import winreg
+    try:
+        registry_paths = []
+        for hive, subkey in [
+            (winreg.HKEY_LOCAL_MACHINE, r"System\CurrentControlSet\Control\Session Manager\Environment"),
+            (winreg.HKEY_CURRENT_USER, r"Environment")
+        ]:
+            try:
+                with winreg.OpenKey(hive, subkey, 0, winreg.KEY_READ) as key:
+                    val, _ = winreg.QueryValueEx(key, "Path")
+                    registry_paths.append(val)
+            except Exception:
+                pass
+        if registry_paths:
+            os.environ["PATH"] = ";".join(registry_paths) + ";" + os.environ.get("PATH", "")
+    except Exception:
+        pass
+
 import yt_dlp
 from pydub import AudioSegment
-import os
+
 
 
 DOWNLOAD_DIR = 'downloades'
-os.makeddirs(DOWNLOAD_DIR,exist_ok = True)
+os.makedirs(DOWNLOAD_DIR, exist_ok=True)
+
 
 def download_youtube_audio(url :str) ->str:
     output_path = os.path.join(DOWNLOAD_DIR, "%(title)s.%(ext)s")
@@ -25,4 +48,18 @@ def download_youtube_audio(url :str) ->str:
         filename = ydl.prepare_filename(info).replace(".webm", ".wav").replace(".m4a", ".wav")
     return filename
 
-print(download_youtube_audio("https://youtu.be/6hV1jnQssj0?si=l0r5vbp_3E9XBNcW"))
+data =download_youtube_audio("https://youtu.be/6hV1jnQssj0?si=l0r5vbp_3E9XBNcW")
+
+
+def convert_to_wav(input_path: str)->str:
+    
+    output_path = os.path.splitext(input_path)[0] + "_converted.wav"
+    audio = AudioSegment.from_file(input_path)
+    audio = audio.set_channels(1).set_frame_rate(16000)
+    audio.export(output_path, format="wav")
+    return output_path
+
+converted_data = convert_to_wav(data)
+print("Converted WAV:", converted_data)
+
+
